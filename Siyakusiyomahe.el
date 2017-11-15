ddfaf08edba1568f39bc0821d38c33e91bb68f6f
Daisuke Inoue <hibariyashiki@gmail.com>
2017-11-15 16:00:15 +0900

    del unnec codes

--- Siyakusiyomahe.el
+++ Siyakusiyomahe.el
@@ -3,54 +3,6 @@
 (setq Sy-newline "
 ")
 
-(setq Sy-2-newlines (s-concat Sy-newline Sy-newline))
-
-(defun S-next-block ()
-    (interactive)
-    (while
-        (not
-            (looking-at "
-
-"))
-        (forward-char))
-    (next-line 2)
-    (beginning-of-line))
-
-(defun s-previous-block ()
-    (interactive)
-    (backward-char)
-    (while
-        (not
-            (looking-back "
-
-"))
-        (backward-char))
-    (previous-line 0)
-    (beginning-of-line))
-
-
-
-
-(defun Sy-n-b ()
-    (interactive)
-    (let*
-        (
-            (newline "
-"))
-        
-        (cond
-            (
-                (looking-at newline)
-                (princ "a")
-                (while
-                    (looking-at newline)
-                    (next-line)))
-            (t
-                (princ "b")
-                (princ "c")
-                (search-forward (s-concat newline newline))))))
-
-
 
 (defun Sy-beginning-of-buffer? ()
     (cond
